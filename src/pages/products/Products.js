eef212bee736bc61f11ad4fa0d85e5809f85b295
sima <siilenus9@gmail.com>
2022-06-18 21:10:36 +0430

    components added

--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -2,6 +2,8 @@ import { Button, Col } from 'react-bootstrap'
 import React from 'react'
 import Wrapper from '../Wrapper'
 import { useProducts, useProductsActions } from './ProductsProvider'
+import Filter from './Filter'
+import Sort from './Sort'
 
 const Products = () => {
     const products = useProducts()
@@ -10,10 +12,15 @@ const Products = () => {
     return (
         <Wrapper linkTitle='مدیریت محصولات'>
             <div className='products-container'>
+                <Col md={6} xs={12} className='products-navbar'>
+                    <Col><Filter /></Col>
+                    <Col><Sort /></Col>
+                </Col>
                 {products.map(item => (
                     <Col md={6} key={item.id} className='product'>
                         <input value={item.name} onChange={(e) => dispatch({type:"EDIT", id: item.id, event: e})} />
                         <span>{item.quantity}</span>
+                        <span>{item.price}</span>
                         <div className='actions'>
                             <Button className='add' onClick={() => dispatch({type:"INCREASE", id: item.id})}>+</Button>
                             {item.quantity === 1 ?
