766b7b0fa78ff0c376595321a890c5479ad3f5a5
sima <siilenus9@gmail.com>
2022-06-18 13:35:16 +0430

    create base of projects

---
+++ src/pages/Wrapper.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const Wrapper = () => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default Wrapper
---
+++ src/pages/home/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import './Homepage.scss'
+
+const HomePage = () => {
+    return (
+        <Container fluid className='homepage'>
+            <Row>
+                
+            </Row>
+        </Container>
+    )
+}
+
+export default HomePage
