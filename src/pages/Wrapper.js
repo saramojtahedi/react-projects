a0c359c491c790571517964194773514f240ace3
sima <siilenus9@gmail.com>
2022-06-18 14:40:57 +0430

    Wrapper page created

--- src/pages/Wrapper.js
+++ src/pages/Wrapper.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import './Wrapper.scss'
 import { Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
-const Wrapper = () => {
+const Wrapper = (props) => {
     return (
-        <Container>
-            
+        <Container className='main-wrapper p-4'>
+            <div className='breadcrumb'>
+                <Link to="/">صفحه اصلی / </Link>
+                <span>{props.linkTitle}</span>
+            </div>
+            {props.children}
         </Container>
     )
 }
