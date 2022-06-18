257fde4e5cdfbb15ef5ad208b0688737d0ee35ac
sima <siilenus9@gmail.com>
2022-06-18 14:41:41 +0430

    products route added

--- src/MainRoutes.js
+++ src/MainRoutes.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/home';
+import Products from './pages/products/Products';
 
 const MainRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="/products" element={<Products />} />
             </Routes>
         </BrowserRouter>
     )
