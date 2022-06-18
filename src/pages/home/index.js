e75f1f4c00a5ca94ba64ad7e2f46d85f49291dba
sima <siilenus9@gmail.com>
2022-06-18 14:42:00 +0430

    products card added to Homepage

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -7,7 +7,12 @@ const HomePage = () => {
     return (
         <Container fluid className='homepage'>
             <Row>
-                
+                <Col xs={12} md={4}>
+                   <Link to='/products'>
+                       <h6>مدیریت محصولات</h6>
+                       <p>مدیریت محصولات با امکان فیلتر، تغییر ترتیب و جستجو</p>
+                   </Link>
+                </Col>
             </Row>
         </Container>
     )
